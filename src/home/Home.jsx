30ca560ed872ddcd0543051e7fd531795010e471
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-03-26 14:56:02 +0000

    style Featured page

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,10 +1,12 @@
 import "./home.scss";
 import Navbar from "../components/navbar/Navbar";
+import Featured from "../components/featured/Featured";
 
 function Home() {
   return (
     <div className="home">
       <Navbar />
+      <Featured />
     </div>
   );
 }
