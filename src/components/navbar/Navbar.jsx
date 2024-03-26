f872142111757655a0316dc92d5945f03508d06f
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-03-26 13:45:22 +0000

    style Navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,18 @@
 import "./navbar.scss";
+import { IoSearchSharp } from "react-icons/io5";
+import { FaBell } from "react-icons/fa6";
+import { IoMdArrowDropdown } from "react-icons/io";
+import { useState } from "react";
 
 function Navbar() {
+  const [hasScrolled, setHasScrolled] = useState(false);
+
+  window.onscroll = () => {
+    setHasScrolled(window.scrollY === 0 ? false : true);
+    return () => (window.onscroll = null);
+  };
   return (
-    <div className="navbar">
+    <div className={hasScrolled ? "navbar scrolled" : "navbar"}>
       <div className="container">
         <div className="left">
           <img
@@ -15,7 +25,24 @@ function Navbar() {
           <span>Popular</span>
           <span>My List</span>
         </div>
-        <div className="right"></div>
+
+        <div className="right">
+          <IoSearchSharp className="icon" />
+          <span>KID</span>
+          <FaBell className="icon" />
+          <img
+            src="https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?q=80&w=1160&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="Side profile of a man with a beard and curly hair."
+          />
+
+          <div className="profile">
+            <IoMdArrowDropdown className="icon" />
+            <div className="options">
+              <span>Settings</span>
+              <span>Logout</span>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
