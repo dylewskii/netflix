63dd1fcf25862d9533cb559322040a87fba95fea
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-03-26 16:09:19 +0000

    add handleSlideClick

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,14 +1,33 @@
+import { useRef } from "react";
 import ListItem from "../list-item/ListItem";
 import "./list.scss";
 import { MdArrowBackIos, MdArrowForwardIos } from "react-icons/md";
 
 function List() {
+  const listRef = useRef();
+
+  const handleSlideClick = (direction) => {
+    // distance from the start of the webpage (X)
+    let distance = listRef.current.getBoundingClientRect().x - 50;
+
+    if (direction === "left") {
+      listRef.current.style.transform = `translateX(${230 + distance}px)`;
+    } else if (direction === "right") {
+      listRef.current.style.transform = `translateX(${-230 + distance}px)`;
+    } else {
+      throw new Error("Incorrect direction");
+    }
+  };
+
   return (
     <div className="list">
       <span className="listTitle">Continue watching</span>
       <div className="wrapper">
-        <MdArrowBackIos className="sliderArrow left" />
-        <div className="container">
+        <MdArrowBackIos
+          className="sliderArrow left"
+          onClick={() => handleSlideClick("left")}
+        />
+        <div className="container" ref={listRef}>
           <ListItem />
           <ListItem />
           <ListItem />
@@ -19,7 +38,10 @@ function List() {
           <ListItem />
           <ListItem />
         </div>
-        <MdArrowForwardIos className="sliderArrow right" />
+        <MdArrowForwardIos
+          className="sliderArrow right"
+          onClick={() => handleSlideClick("right")}
+        />
       </div>
     </div>
   );
