c44bbaa8ac91c6cc8cb5dd7afed3b6829696874a
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-03-27 12:10:33 +0000

    add ListItem component

--- src/components/list-item/ListItem.jsx
+++ src/components/list-item/ListItem.jsx
@@ -1,7 +1,54 @@
+/* eslint-disable react/prop-types */
 import "./listItem.scss";
+import { IoMdPlay, IoMdAdd } from "react-icons/io";
+import { AiOutlineLike, AiOutlineDislike } from "react-icons/ai";
+import { useState } from "react";
 
-function ListItem() {
-  return <div className="listItem"></div>;
+function ListItem({ index }) {
+  const [isHovered, setIsHovered] = useState(false);
+  const trailer =
+    "https://player.vimeo.com/external/371433846.sd.mp4?s=236da2f3c0fd273d2c6d9a064f3ae35579b2bbdf&profile_id=139&oauth2_token_id=57447761";
+
+  return (
+    <div
+      className="listItem"
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+      // prevent listItem container from shifting to the left
+      style={{ left: isHovered && index * 225 - 50 + index * 2.5 }}
+    >
+      <img
+        src="https://occ-0-1723-92.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABU7D36jL6KiLG1xI8Xg_cZK-hYQj1L8yRxbQuB0rcLCnAk8AhEK5EM83QI71bRHUm0qOYxonD88gaThgDaPu7NuUfRg.jpg?r=4ee"
+        alt=""
+      />
+
+      {isHovered && (
+        <>
+          <video src={trailer} autoPlay={true} loop></video>
+          <div className="itemInfo">
+            <div className="icons">
+              <IoMdPlay className="icon" />
+              <IoMdAdd className="icon" />
+              <AiOutlineLike className="icon" />
+              <AiOutlineDislike className="icon" />
+            </div>
+            <div className="top">
+              <span>2hrs 20mins</span>
+              <span className="ageLimit">18+</span>
+              <span>2007</span>
+            </div>
+            <div className="desc">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              Voluptates quasi delectus facilis mollitia quam, distinctio
+              dignissimos nostrum numquam culpa nam? Quasi enim in ea dolor
+              provident accusantium, facere pariatur nulla.
+            </div>
+            <div className="genre">Action</div>
+          </div>
+        </>
+      )}
+    </div>
+  );
 }
 
 export default ListItem;
