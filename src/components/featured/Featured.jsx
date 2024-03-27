2b6645b3a61dfb4a45f7954d1ffdcab4864e94af
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-03-27 14:38:27 +0000

    add featured image

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -27,15 +27,16 @@ function Featured({ type }) {
           </select>
         </div>
       )}
+
       <img
-        src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
-        alt=""
+        src="https://images.pexels.com/photos/8718381/pexels-photo-8718381.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+        alt="A man pointing two guns to each side"
       />
 
       <div className="info">
         <img
           src="https://occ-0-1432-1433.1.nflxso.net/dnm/api/v6/LmEnxtiAuzezXBjYXPuDgfZ4zZQ/AAAABUZdeG1DrMstq-YKHZ-dA-cx2uQN_YbCYx7RABDk0y7F8ZK6nzgCz4bp5qJVgMizPbVpIvXrd4xMBQAuNe0xmuW2WjoeGMDn1cFO.webp?r=df1"
-          alt=""
+          alt="A man with a confused look on his face"
         />
         <span className="description">
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga culpa
