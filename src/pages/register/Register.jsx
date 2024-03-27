9504477928f0833f90360d806b60ce853b3c2254
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-03-27 13:54:55 +0000

    implement register page

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,5 +1,59 @@
+import { useRef, useState } from "react";
 import "./register.scss";
+import { MdKeyboardArrowRight } from "react-icons/md";
 
 export default function Register() {
-  return <div className="register"></div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+  const handleStart = () => {
+    setEmail(emailRef.current.value);
+  };
+  const handleFinish = () => {
+    setPassword(passwordRef.current.value);
+  };
+  return (
+    <div className="register">
+      <div className="top">
+        <div className="wrapper">
+          <img
+            className="logo"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"
+            alt=""
+          />
+          <button className="loginButton">Sign In</button>
+        </div>
+      </div>
+      <div className="container">
+        <h1>Unlimited films, TV programmes and more</h1>
+        <h2>Watch anywhere. Cancel at any time.</h2>
+        <p>
+          Ready to watch? Enter your email to create or restart your membership.
+        </p>
+        {!email ? (
+          <div className="input">
+            <input
+              type="email"
+              placeholder="Email address"
+              ref={emailRef}
+              className="emailInput"
+            />
+            <button className="registerButton" onClick={handleStart}>
+              Get Started <MdKeyboardArrowRight className="rightArrow" />
+            </button>
+          </div>
+        ) : (
+          <form className="input">
+            <input type="password" placeholder="password" ref={passwordRef} />
+            <button className="registerButton" onClick={handleFinish}>
+              Start
+            </button>
+          </form>
+        )}
+      </div>
+    </div>
+  );
 }
