c1faa49a2922dc3442975360d506cbaec33bc2a7
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-03-27 14:34:40 +0000

    render Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import "./app.scss";
 import Home from "./pages/home/Home";
-import Register from "./pages/register/Register";
-import Login from "./pages/login/Login";
-import Watch from "./pages/watch/Watch";
+// import Register from "./pages/register/Register";
+// import Login from "./pages/login/Login";
+// import Watch from "./pages/watch/Watch";
 
 function App() {
   return (
     <>
-      <Login />
+      <Home />
     </>
   );
 }
