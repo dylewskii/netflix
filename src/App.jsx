2d34124dbcd712a0d4d652cddfbb5d038344dbba
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-03-27 14:28:03 +0000

    implement login component w/ styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import "./app.scss";
 import Home from "./pages/home/Home";
 import Register from "./pages/register/Register";
+import Login from "./pages/login/Login";
 import Watch from "./pages/watch/Watch";
 
 function App() {
   return (
     <>
-      <Register />
+      <Login />
     </>
   );
 }
