895fdfbbf24bbc24e7ed491bff9d9636950d0b81
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-03-28 12:36:13 +0000

    add Movie Schema

---
+++ backend/models/Movie.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const MovieSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    description: { type: String },
+    releaseYear: { type: String },
+    img: { type: String },
+    imgTitle: { type: String },
+    imgSmall: { type: String },
+    trailer: { type: String },
+    video: { type: String },
+    genre: { type: String },
+    ageLimit: { type: Number },
+    isSeries: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Movie", MovieSchema);
