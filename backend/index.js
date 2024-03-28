eb02512b488aeb73e1e27917f964709dda6ead1b
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-03-28 12:24:00 +0000

    connect to db

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,12 @@
+import "dotenv/config.js";
 import express from "express";
+import mongoose from "mongoose";
+
 const app = express();
+mongoose
+  .connect(process.env.DB_URI)
+  .then(() => console.log("DB Connected"))
+  .catch((err) => console.log(err));
 
 const port = process.env.PORT || 3000;
 
