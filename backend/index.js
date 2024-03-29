4df8c5846d44aa2f295de1133b830d6598c9f6ba
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-03-29 14:22:57 +0000

    set up login route /w authentication

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 // routes
 const authRouter = require("./routes/auth");
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 const app = express();
 mongoose
   .connect(process.env.DB_URI)
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,16 @@
 /* eslint-disable no-unused-vars */
 const User = require("../models/User");
 const router = require("express").Router();
+const CryptoJS = require("crypto-js");
 
 router.post("/register", async (req, res) => {
   const newUser = new User({
     username: req.body.username,
     email: req.body.email,
-    password: req.body.password,
+    password: CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.SECRET_KEY
+    ).toString(),
   });
 
   try {
@@ -14,6 +18,37 @@ router.post("/register", async (req, res) => {
     res.status(201).json(user);
   } catch (error) {
     res.status(500).json(error);
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+
+    // User not found
+    if (!user) {
+      res.status(401).json({ msg: "Wrong password or username", code: 401 });
+    }
+
+    const bytes = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY);
+    const originalPassword = bytes.toString(CryptoJS.enc.Utf8);
+
+    // Password mismatch
+    if (originalPassword !== req.body.password) {
+      res.status(401).json({ msg: "Wrong password or username", code: 401 });
+    }
+
+    // Omit the password from the user object before sending it back
+    const { password, ...userData } = user._doc;
+
+    res.status(200).json({ user: userData });
+  } catch (error) {
+    // res.status(500).json(error);
+    if (!res.headersSent) {
+      res
+        .status(500)
+        .json({ message: "An error occurred during the login process." });
+    }
   }
 });
 
